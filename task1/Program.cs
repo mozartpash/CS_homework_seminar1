f3fda60f8fbad884b478c86e8e5d2732bc3d05cd
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-07 21:57:58 +0300

    Made task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,3 +2,16 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine()!);
+if(number1 > number2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{
+    Console.WriteLine("Первое число меньше второго");
+}
