0b3a8aa9cfb1e72b9f51a465178e1554a7f89afa
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-10 12:01:12 +0300

    task2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -13,19 +13,19 @@ Console.WriteLine("Введите третье число ");
 int number3 = int.Parse(Console.ReadLine()!);
 //Console.WriteLine(number3);
 int max = number1;
-    if(max > number2)
+    if(max < number2)
     {
         max = number2;
     }
     
-    if(max > number3)
+    if(max < number3)
     {
         max = number3;
+        Console.WriteLine("Максимальное число " + max);
     }
     
 else
 {
-Console.WriteLine("Нет максимального числа");
+Console.WriteLine("Нет одного максимального числа");
 }
-Console.WriteLine("Максимальное число " + max);
-//неверный алгоритм, не правильно выводит результат
+
